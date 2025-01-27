858dcc53747ef2bd3e4c0cfeca48860e1102a38a
Foster Adu-Gyamfi <113718153+Foxynero@users.noreply.github.com>
2025-01-27 18:36:02 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // for React 18 and above
-import App from './App'; // Main app component
-import { BrowserRouter as Router } from 'react-router-dom'; // If you're using React Router
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// Find the root element in index.html where the app will be rendered
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Render the main App component inside the root
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
